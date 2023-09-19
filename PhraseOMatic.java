d3e01c14a09b248797c04703d4918f9d8de4c05c
MICcall2004 <sseony88@gmail.com>
2023-09-20 06:57:46 +0900

    3주차 수업 내용

---
+++ PhraseOMatic.java
@@ -0,0 +1,9 @@
+public class PhraseOMatic {
+    public static void main(String[] args) {
+        String[] wordListOne = {"24/7", "multi-Tier", "30,000 foot", "B-to-B",
+                                "win-win", "front-end", "web-based", "pervasive", "smart", "six-sigma",
+                                "critical-path", "dynamic"};
+
+        String[] wordListTwo
+    }
+}
